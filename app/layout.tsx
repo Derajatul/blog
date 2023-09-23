ecdc4aabd3739b815627b21849d77a335e778e33
Dera <derajatul@gmail.com>
2023-09-23 18:49:07 +0700

    add darkmode feature ✅

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,34 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Patrick_Hand } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Patrick_Hand } from "next/font/google";
+import Nav from "../components/ui/Nav";
+import { ThemeProvider } from "../components/theme-provider";
 
-const patrickHand = Patrick_Hand({weight: "400",subsets: ["latin"]})
+const patrickHand = Patrick_Hand({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Dera',
-  description: 'Blog',
-}
+  title: "Dera",
+  description: "Blog",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={patrickHand.className}>{children}</body>
+      <body className={`${patrickHand.className} dark:pp-background`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Nav />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
