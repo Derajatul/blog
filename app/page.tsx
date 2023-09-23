331fcd9fe55ff48dcd063d7a764efaab873e5948
Dera <derajatul@gmail.com>
2023-09-23 20:34:36 +0700

    update card blog component ✅

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,42 @@
 import CardBlog from "@/components/ui/card-blog";
+import { showFormattedDate } from "@/lib/utils";
 const CardBlogList = () => {
   const datas = [
     {
+      id: 1,
       imgUrl:
         "https://www.genatec.com/hs-fs/hubfs/genatec_blog_assets/Blog-2022-005/What%20is%20a%20gaming%20setup_-1.jpg?width=1477&height=831&name=What%20is%20a%20gaming%20setup_-1.jpg",
       title: "The More Important the Work, the More Important the Rest",
       description:
         "Suitable Quality is determined by product users, clients or customers, not by society in general. For example, a low priced product may be viewed as having high.",
+      date: new Date(),
+      category: "tech",
     },
     {
+      id: 1,
       imgUrl:
         "https://www.genatec.com/hs-fs/hubfs/genatec_blog_assets/Blog-2022-005/What%20is%20a%20gaming%20setup_-1.jpg?width=1477&height=831&name=What%20is%20a%20gaming%20setup_-1.jpg",
       title: "The More Important the Work, the More Important the Rest",
       description:
         "Suitable Quality is determined by product users, clients or customers, not by society in general. For example, a low priced product may be viewed as having high.",
+      date: new Date(),
+      category: "tech",
     },
     {
+      id: 1,
       imgUrl:
         "https://www.genatec.com/hs-fs/hubfs/genatec_blog_assets/Blog-2022-005/What%20is%20a%20gaming%20setup_-1.jpg?width=1477&height=831&name=What%20is%20a%20gaming%20setup_-1.jpg",
       title: "The More Important the Work, the More Important the Rest",
       description:
         "Suitable Quality is determined by product users, clients or customers, not by society in general. For example, a low priced product may be viewed as having high.",
-    },
-    {
-      imgUrl:
-        "https://www.genatec.com/hs-fs/hubfs/genatec_blog_assets/Blog-2022-005/What%20is%20a%20gaming%20setup_-1.jpg?width=1477&height=831&name=What%20is%20a%20gaming%20setup_-1.jpg",
-      title: "The More Important the Work, the More Important the Rest",
-      description:
-        "Suitable Quality is determined by product users, clients or customers, not by society in general. For example, a low priced product may be viewed as having high.",
-    },
-    {
-      imgUrl:
-        "https://www.genatec.com/hs-fs/hubfs/genatec_blog_assets/Blog-2022-005/What%20is%20a%20gaming%20setup_-1.jpg?width=1477&height=831&name=What%20is%20a%20gaming%20setup_-1.jpg",
-      title: "The More Important the Work, the More Important the Rest",
-      description:
-        "Suitable Quality is determined by product users, clients or customers, not by society in general. For example, a low priced product may be viewed as having high.",
-    },
-    {
-      imgUrl:
-        "https://www.genatec.com/hs-fs/hubfs/genatec_blog_assets/Blog-2022-005/What%20is%20a%20gaming%20setup_-1.jpg?width=1477&height=831&name=What%20is%20a%20gaming%20setup_-1.jpg",
-      title: "The More Important the Work, the More Important the Rest",
-      description:
-        "Suitable Quality is determined by product users, clients or customers, not by society in general. For example, a low priced product may be viewed as having high.",
+      date: new Date(),
+      category: "tech",
     },
   ];
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-[30px]">
       {datas.map((data, index) => (
-        <CardBlog {...data} key={index} />
+        <CardBlog {...data} date={showFormattedDate(data.date)} key={index} />
       ))}
     </div>
   );
--- components/ui/card-blog.tsx
+++ components/ui/card-blog.tsx
@@ -14,14 +14,24 @@ type CardBlogProps = {
   imgUrl: string;
   title: string;
   description: string;
+  date: string;
+  id: number;
+  category: string;
 };
 
-const CardBlog = ({ imgUrl, title, description }: CardBlogProps) => {
+const CardBlog = ({
+  imgUrl,
+  title,
+  description,
+  date,
+  id,
+  category,
+}: CardBlogProps) => {
   return (
     <Link href="example.com">
       <Card>
         <CardHeader>
-          <figure className="w-full max-h-[213px] overflow-hidden rounded-lgcmb-[21px]">
+          <figure className="w-full max-h-[213px] overflow-hidden rounded-lg mb-[21px]">
             <Image
               src={imgUrl}
               alt="placeholder"
@@ -30,7 +40,11 @@ const CardBlog = ({ imgUrl, title, description }: CardBlogProps) => {
               className="bg-cover "
             />
           </figure>
-
+          <CardDescription className="flex gap-4">
+            <span>{category}</span>
+            <span>|</span>
+            <span>{date}</span>
+          </CardDescription>
           <CardTitle className="text-2xl mb-[10px] leading-[38px]">
             {title}
           </CardTitle>
